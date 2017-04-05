7250628adea2ac12ec3effcc0ba64eb97e89f984
juan <vilfrack21@gmail.com>
2017-04-04 21:54:10 -0400

    26. Se crea un controlador,vista y vistamodel nuevo

---
+++ Z-Market/Z-Market/ViewModels/RolView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Z_Market.ViewModels
+{
+    public class RolView
+    {
+        public string RoleID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Z-Market/Z-Market/ViewModels/UserView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Z_Market.ViewModels;
+
+namespace Z_Market.Models
+{
+    public class UserView
+    {
+        public string UserID { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        //sirve para pintar los roles en la vista
+        public RolView Role { get; set; }
+
+        public List<RolView> Roles { get; set; }
+    }
+}
